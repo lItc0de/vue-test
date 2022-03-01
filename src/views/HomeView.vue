d730c50133d77413167c6544b3f96d76563756f6
Jolyon Pawlyn <jolyon@brella.io>
2022-03-01 17:38:11 +0300

    List hard coded recipes on home page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,29 @@
-<script setup></script>
+<script setup>
+const recipes = [
+  {
+    title: "Apple pie",
+    description: "Just like your Mums",
+    author: "Joe Bloggs",
+    dateCreated: new Date(2022, 1, 1),
+  },
+  {
+    title: "Bratwurst",
+    description: "The best in Kreuzberg",
+    author: "Hans Solo",
+    dateCreated: new Date(2021, 8, 31),
+  },
+];
+</script>
 
 <template>
   <main>
-    <h2>Recipe app</h2>
+    <h2>Recipe list</h2>
+    <ul>
+      <li v-for="recipe in recipes" :key="recipe.title">
+        <p>{{ recipe.title }}</p>
+        <p>{{ recipe.description }}</p>
+        <p>Created by {{ recipe.author }} on {{ recipe.dateCreated }}</p>
+      </li>
+    </ul>
   </main>
 </template>
